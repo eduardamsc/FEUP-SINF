bd05d77b9335e5230f1a9c2d5b40bdbfbded21c7
marianasilva14 <mariana16095@gmail.com>
2018-11-19 10:10:02 +0000

    Try to fetch data from ERP

--- web-app/src/components/Wave.js
+++ web-app/src/components/Wave.js
@@ -2,7 +2,66 @@ import React, { Component } from 'react';
 import { Table, NavLink } from 'reactstrap';
 import { NavLink as RRNavLink } from 'react-router-dom';
 
+const API_ENDPOINT = 'http://localhost:2018/WebApi';
+
+export function makeRequestBody(body) {
+    let formData = new URLSearchParams();
+
+    for (var key in body) {
+      formData.append(key, body[key]);
+    }
+
+    return formData;
+};
+
+export function authenticate() {
+    return fetch(`${API_ENDPOINT}/token`, {
+        method: 'POST',
+        body: makeRequestBody(
+            {
+                username: 'FEUP',
+                password: 'qualquer1',
+                company: 'demo',
+                instance: 'DEFAULT',
+                line: 'professional',
+                grant_type: 'password'
+            }
+        ),
+    });
+}
+
 class Wave extends Component {
+
+  constructor(props) {
+      super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            //projects: []
+        };
+
+    }
+
+  componentDidMount() {
+      const route = API_ENDPOINT;
+      fetch(route)
+          .then(res => res.json())
+          .then(
+              (result) => {
+                  this.setState({
+                      isLoaded: true,
+                  });
+              },
+              (error) => {
+                  this.setState({
+                      isLoaded: true,
+                      error
+                  });
+              }
+          );
+
+  }
+
     render() {
         return (
           <div>

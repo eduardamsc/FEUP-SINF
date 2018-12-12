43ddbe0f638c369e7eec424a23b74846a256729c
FranciscaCerquinho <franciscacerquinho@gmail.com>
2018-12-12 17:21:01 +0000

    trying to do login

--- react-express/client/src/components/SignIn.js
+++ react-express/client/src/components/SignIn.js
@@ -2,41 +2,78 @@ import React, { Component } from 'react';
 import { Container, Col, Form, FormGroup, Input, Button } from 'reactstrap';
   
 class SignIn extends Component {
+
     constructor(props) {
         super(props);
           this.state = {
           id:'',
-          name: '',
+          username: '',
           password: '',
+          user:''
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+      }
+
+    /*login(username, password){
+      return{
+        request: {
+          url:'https://localhost:5000/login',
+          data: {
+            username: username,
+            password: password
+          }
         }
-        this.handleChange = this.handleChange.bind(this);
       }
+    }*/
 
-    componentDidMount(){
-      fetch('/login')
-      .then(res => res.json())
-      .then(
-        (result)
+    onSubmit(){
+     // const { username, password } = this.state;
+      console.log("Clicou!");
+     // this.login(username, password);
+     fetch("/login", {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: {
+        "username": this.state.username,
+        "password": this.state.password
+       }
+
+    })
+      .then(res => console.log(res)
       )
+/*      .then(
+        (result) => {
+          console.log(result);
+          
+          this.setState({
+            user: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )*/
     }
-    
-    
-      handleChange = async (event) => {
-        const { target } = event;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const { name } = target;
-        await this.setState({
-          [ name ]: value,
-        });
-      }
 
-      handleLogin(){
-        this.setState()
-      }
+    handleInputChange(event){
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+    }
 
     render() {
+      const { user } = this.state;
+
         return (
-          <Container className="App">
+         <Container className="App">
             <h2>Sign In</h2>
             <Form className="form col-12">
               <Col>
@@ -46,6 +83,8 @@ class SignIn extends Component {
                     name="username"
                     id="exampleUsername"
                     placeholder="Username"
+                    value={this.state.username}
+                    onChange={this.handleInputChange}
                   />
                 </FormGroup>
               </Col>
@@ -56,10 +95,12 @@ class SignIn extends Component {
                     name="password"
                     id="examplePassword"
                     placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleInputChange}
                   />
                 </FormGroup>
               </Col>
-              <Button className="btn btn-outline-secondary btn-lg" onClick={this.handleLogin}>Sign In</Button>
+              <Button className="btn btn-outline-secondary btn-lg" onClick={this.onSubmit}>Sign In</Button>
             </Form>
           </Container>
         );

52854be7eea108e4a68989afabca672622f49829
marianasilva14 <mariana16095@gmail.com>
2018-12-14 10:21:01 +0000

    fixed visit card

--- react-express/client/src/components/Home.js
+++ react-express/client/src/components/Home.js
@@ -9,14 +9,13 @@ class Home extends Component {
         return (
             <Container className="Welcome">
             <div className="description ">
-                <h1>    Hello ,Welcome To Warehouse 
-                    <p>   Are you a picker? We are going to help you improve your work.
-                </p>   
+                <h1>    Hello, Welcome To Warehouse!
+                    <p>   Are you a picker? We are going to help you improve your work.</p>
                 <Button href="SignIn" className="btn btn-outline-secondary btn-lg">Sign In</Button>
-                   </h1>  
+                   </h1>
             </div>
             </Container>
-           
+
         );
     }
 }
